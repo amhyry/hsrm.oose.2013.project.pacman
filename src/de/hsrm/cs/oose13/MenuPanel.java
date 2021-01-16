4a69aaf635f6fbb2fcd705ab766debdb5b456f18
Arnold <arnold.riemer@googlemail.com>
2021-01-16 14:52:57 +0100

    update

--- src/de/hsrm/cs/oose13/MenuPanel.java
+++ src/de/hsrm/cs/oose13/MenuPanel.java
@@ -1,18 +1,13 @@
 package de.hsrm.cs.oose13;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import de.hsrm.cs.oose13.basics.ImagePanel;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-
-import de.hsrm.cs.oose13.basics.ImagePanel;
 /**
  * The ImagePanel Class represents a Panel with a single Image.
  * @author      Arnold Riemer <arnold.riemer@gmail.com>
